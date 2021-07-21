6f63b00944d6142e4baeb72ac70fcd9d99fea398
Joshua A. Díaz Robayna <66664769+jadrdev@users.noreply.github.com>
2021-07-21 15:37:22 +0100

    Cambios y actualización de RN

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import { View, Text, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import Navigation from './src/navigation/Navigation';
 
 export default function App() {
